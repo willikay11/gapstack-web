a42a974ac0efba7611bdff4529f4dc6e7de5d497
willikay11 <willikay11@gmail.com>
2020-09-20 14:54:20 +0300

    Added menu grids

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -5,15 +5,23 @@ import gapstackLogo from '../../assets/gapStackLogo.svg';
 const Menu = () => {
     return(
         <Row style={{ height: 130, alignItems: 'center', padding: '20px 0px', position: 'relative', zIndex: 10 }}>
-            <Col offset={4} span={12} style={{ display: 'flex', alignItems: 'center'}}>
+            <Col xs={{ span: 10, offset: 2 }}
+                 sm={{ span: 10, offset: 2 }}
+                 md={{ span: 10, offset: 2 }}
+                 lg={{ span: 8, offset: 4 }}
+                 xl={{ span: 8, offset: 4 }} style={{ display: 'flex', alignItems: 'center'}}>
                 <img height={25} src={gapstackLogo} alt='logo'/>
             </Col>
-            <Col span={8}>
+            <Col xs={{ span: 10, offset: 2 }}
+                 sm={{ span: 10, offset: 2 }}
+                 md={{ span: 10, offset: 2 }}
+                 lg={{ span: 8, offset: 2 }}
+                 xl={{ span: 8, offset: 2 }}>
                 <Row gutter={32} style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
-                    <Col span={4}>
+                    <Col xs={9} sm={9} md={4} lg={4} xl={6}>
                         <Button type='link' style={{ color: '#39426A'}}>Solutions</Button>
                     </Col>
-                    <Col span={4}>
+                    <Col xs={12} sm={12} md={4} lg={4} xl={6}>
                         <Button style={{ color: '#fff', backgroundColor: '#7c60ff', height: 50, borderRadius: 4}}> Contact Sales</Button>
                     </Col>
                 </Row>
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,4 @@
 import React from "react";
-import Menu from "../../Components/Menu";
-import { Button, Col, Row } from "antd";
-import { Fade } from "react-awesome-reveal";
-import Particles from "react-particles-js";
 import styled from 'styled-components';
 import Who from "../Components/Who";
 import Why from "../Components/Why";
@@ -13,6 +9,7 @@ import Dashboard from "../Components/Dashboard";
 import Users from "../Components/Users";
 import Footer from "../Components/Footer";
 import Contact from "../Components/Contact";
+import Hero from "../Components/HeroBanner";
 
 const HomeWrapper = styled.div`
   ${style}
@@ -22,58 +19,7 @@ const Home = () => {
     return (
         <HomeWrapper>
             <div className="home-container">
-                <Row style={{ position: 'relative' }}>
-                    <Col span={24}>
-                        <Menu />
-
-                        <Fade style={{ position: 'relative', zIndex: 10 }} triggerOnce={true} fraction={0.3}>
-                            <Row style={{ height: 800}}>
-                                <Col offset={4} span={16}>
-                                    <Row style={{ height: '100%' }}>
-                                        <Col span={16} style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'center', flexDirection: 'column' }}>
-                                            <p style={{ fontSize: 44, color: '#39426A', textTransform: 'capitalize', lineHeight: '135.2%', fontWeight: 'bold', fontStyle: 'normal' }}>powerful digital trade & supply chain financing platform</p>
-
-                                            <Row>
-                                                <Col span={15}>
-                                                    <p style={{ fontSize: 16, color: '#39426A', lineHeight: '30px', fontWeight: 500, fontStyle: 'normal' }}>When you register as a supplier, TradeDepot becomes a full-time distributor of your products. We buy and store the inventory, and take care of everything from shipping and pricing to customer service and returns.</p>
-                                                </Col>
-                                            </Row>
-                                            <Button style={{ color: '#fff', backgroundColor: '#7c60ff', height: 50, borderRadius: 4, marginTop: 50 }}>Contact Sales</Button>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Fade>
-
-                        <Particles
-                            style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 0 }}
-                            params={{
-                                particles: {
-                                    number: {
-                                        value: 80,
-                                        density: {
-                                            enable: true,
-                                        }
-                                    },
-                                    links: {
-                                        color: "#b3b2b2",
-                                        distance: 150,
-                                        enable: true,
-                                        opacity: 0.5,
-                                        width: 1,
-                                    },
-                                    shape: {
-                                        type: "circle",
-                                    },
-                                    size: {
-                                        random: true,
-                                        value: 5,
-                                    },
-                                },
-                            }}
-                        />
-                    </Col>
-                </Row>
+                <Hero />
 
                 <Who />
 
