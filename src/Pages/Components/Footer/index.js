88b6de1f094ed2160e49818b431b8d0e29fd0419
willikay11 <willikay11@gmail.com>
2020-09-23 12:30:45 +0300

    Updated medium screen sizes

--- src/Pages/Components/Footer/index.js
+++ src/Pages/Components/Footer/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Fade} from "react-awesome-reveal";
 import { Col, Row } from "antd";
 import gapstackLogo from "../../../assets/gapStackLogo.svg";
-import {MdCopyright} from "react-icons/md";
 import styled from "styled-components";
 import { style } from "./styles";
 
@@ -17,11 +16,11 @@ const Footer = ({ scrollToSolutions, scrollToContactForm }) => {
                 <Row className="container">
                     <Col xs={{ span:22, offset:2 }}
                          sm={{ span:22, offset:2 }}
-                         md={{ span:22, offset:2 }}
-                         lg={{ span:20, offset:4 }}
-                         xl={{ span:20, offset:4 }}>
+                         md={{ span:20, offset:2 }}
+                         lg={{ span:16, offset:4 }}
+                         xl={{ span:16, offset:4 }}>
                         <Row gutter={[16, 32]}>
-                            <Col xs={24} sm={24} md={24} lg={6} xl={6} className="item">
+                            <Col xs={24} sm={24} md={24} lg={24} xl={6} className="item">
                                 <Row style={{ marginBottom: 10 }}>
                                     <img height={40} src={gapstackLogo} alt='alternate'/>
                                 </Row>
@@ -31,7 +30,7 @@ const Footer = ({ scrollToSolutions, scrollToContactForm }) => {
                                 </Row>
                             </Col>
 
-                            <Col xs={24} sm={12} md={12} lg={6} xl={6} className="item">
+                            <Col xs={24} sm={12} md={12} lg={12} xl={6} className="item">
                                 <p className="title">Kenya</p>
                                 <p className="text">NAIROBI – HEAD OFFICE</p>
                                 <p className="text">Kirichwa Rd, Nairobi</p>
@@ -40,14 +39,14 @@ const Footer = ({ scrollToSolutions, scrollToContactForm }) => {
                                 <p className="email-link">info@gapstack.com</p>
                             </Col>
 
-                            <Col xs={24} sm={12} md={12} lg={6} xl={6} className="item">
+                            <Col xs={24} sm={12} md={12} lg={12} xl={6} className="item">
                                 <p className="title">USA</p>
                                 <p className="text">SAN FRANCISCO</p>
                                 <p className="text">39 East 8th St.</p>
                                 <p className="text"> San Francisco, CA 94110</p>
                             </Col>
 
-                            <Col xs={24} sm={12} md={12} lg={6} xl={6} className="item">
+                            <Col xs={24} sm={12} md={12} lg={12} xl={6} className="item">
                                 <p className="title">India</p>
                                 <p className="text">NEW DELHI</p>
                                 <p className="text">Gwalior, M.P</p>
@@ -63,7 +62,7 @@ const Footer = ({ scrollToSolutions, scrollToContactForm }) => {
                          md={{ span:22, offset:2 }}
                          lg={{ span:20, offset:4 }}
                          xl={{ span:20, offset:4 }} style={{ display: 'flex', alignItems: 'center' }}>
-                        <p><MdCopyright /> Gapstack 2020. All Right Reserved</p>
+                        <p>Gapstack 2020. All Right Reserved</p>
                     </Col>
                 </Row>
             </Fade>
